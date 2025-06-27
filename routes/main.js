a13419727c58ee04788aea162c8657a14c81262b
Emanuel <joaoemanuelribeiromarinho@gmail.com>
2025-06-27 00:15:47 -0300

    refatorando js

--- routes/main.js
+++ routes/main.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.sendFile('index.html', { root: './public' });
+  res.sendFile('index.html', { root: '.' });
 });
 
 router.post('/enviar-feedback', (req, res) => {
